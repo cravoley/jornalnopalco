5ec7025bb80497dd65ec71afdcc7de7468e21fa7
Valdo Raya <cravoley@gmail.com>
2016-04-27 21:26:13 -0300

    Prepare bower and gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,76 @@ var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
-var minifyCss = require('gulp-minify-css');
+var minifyCss = require('gulp-clean-css');
+var googlecdn = require('gulp-google-cdn');
+var sass = require('gulp-sass');
+var del = require('del');
 
 
-var cssOptions = {};
-var jsOptions = {};
+var devTasks = ['clean', 'compress-js-dev', 'sass-dev'];
+var prodTasks = ['clean', 'compress-js', 'sass', 'minify-css', 'cdn'];
+
 
 gulp.task('default', ['dev'], function () {
 });
+gulp.task('clean', function () {
+	return del([
+		'dist/**/*.css',
+		'dist/**/*.scss',
+		'dist/**/*.js',
+	]);
+});
 
+
+//dev
+gulp.task("compress-js-dev", function () {
+	return gulp.src('js/*.js')
+		.pipe(uglify(
+			{
+				"compress": false,
+				"preserveComments": "all",
+				"output": {
+					"beautify": true,
+					"comments": true,
+					"bracketize": true
+				}
+			}
+		))
+		.pipe(concat('all.js'))
+		.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('sass-dev', function () {
+	return gulp.src('sass/**/*.scss') // path to your file
+		.pipe(sass(
+			{
+				outputStyle: 'expanded',
+				sourceComments: true
+			}
+		)).pipe(minifyCss(
+			{
+				"benchmark": true,
+				"compatibility": 'ie8',
+				"keepSpecialComments": "*",
+				"keepBreaks": true
+			}
+		))
+		//.pipe(concat('all.css'))
+		.pipe(gulp.dest('dist/css'));
+});
+
+// prod
 gulp.task("compress-js", function () {
 	return gulp.src('js/*.js')
 		.pipe(uglify())
 		.pipe(concat('all.js'))
 		.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('cdn', function () {
+	return gulp.src('index.html')
+		.pipe(googlecdn(require('./bower.json')))
+		.pipe(gulp.dest('dist'));
 });
 
 gulp.task('minify-css', function () {
@@ -24,11 +80,22 @@ gulp.task('minify-css', function () {
 		.pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('dev', ['compress-js', 'minify-css'], function () {
+gulp.task('sass', function () {
+	return gulp.src('sass/**/*.scss') // path to your file
+		.pipe(sass(
+			{
+				outputStyle: 'expanded',
+				sourceComments: true
+			}
+		))
+		.pipe(minifyCss())
+		.pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('dev', devTasks, function () {
+	gulp.watch(['css/**/*.css', 'js/**/*.js', 'sass/**/*.scss'], devTasks);
+});
+
+gulp.task('prod', prodTasks, function () {
 	cssOptions = {};
-	jsOptions = {
-		"preserveComments": "all",
-		"beautify": true
-	};
-	gulp.watch(['css/**/*.css', 'js/**/*.js'], ['compress-js', 'minify-css']);
 });
