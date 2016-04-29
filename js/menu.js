09e3ea01f3d271546ceff5f29bd92b1321027b7a
Valdo Raya <cravoley@gmail.com>
2016-04-29 20:52:37 -0300

    Improve menu behaviour

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,5 @@
 $(document).on("scroll", function () {
-	if ($(document).scrollTop() > 100) {
+	if ($(document).scrollTop() > 20) {
 		$("header").removeClass("largeMenu").addClass("smallMenu");
 	} else {
 		$("header").removeClass("smallMenu").addClass("largeMenu");
