a4d4f6ff74536ab070c31f178800717495867414
Valdo Raya <cravoley@gmail.com>
2016-05-03 00:31:51 -0300

    Move index.html to index.php so it's possible to use include. Thus, improving the file's readability

---
+++ events/shows.php
@@ -0,0 +1,25 @@
+<section>
+    <div class="col-sm-4">
+        <div class="show border">
+            <h2 class="title">Show</h2>
+            <ul class="list-unstyled">
+                <?php
+                    for($i=0;$i<6;$i++){
+                        ?>
+
+                        <li>
+                           <a href="#">
+                               <time class="data">22/03 - </time>
+                               <span class="name">Evento <?php echo $i+1;?></span>
+                           </a>
+                        </li>
+                        <?php
+                    }
+                 ?>
+            </ul>
+            <div class="more pull-right">
+                <a href="#">Ver mais</a>
+            <div>
+        </div>
+    </div>
+</section>
--- index.php
+++ index.php
@@ -41,147 +41,18 @@
 </header>
 <div class="">
 	<div class="container content">
-		<div class="row">
-			<div class="col-xs-12">
-				<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-					<!-- Indicators -->
-					<ol class="carousel-indicators">
-						<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-						<li data-target="#carousel-example-generic" data-slide-to="1"></li>
-						<li data-target="#carousel-example-generic" data-slide-to="2"></li>
-					</ol>
 
-					<!-- Wrapper for slides -->
-					<div class="carousel-inner" role="listbox">
-						<div class="item active">
-							<img src="http://lorempixel.com/400/200/" alt="...">
-
-							<div class="carousel-caption">
-								Oi 1
-							</div>
-						</div>
-						<div class="item">
-							<img src="http://lorempixel.com/400/200/" alt="...">
-
-							<div class="carousel-caption">
-								po2
-							</div>
-						</div>
-						...
-					</div>
-
-					<!-- Controls -->
-					<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-						<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-						<span class="sr-only">Previous</span>
-					</a>
-					<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-						<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-						<span class="sr-only">Next</span>
-					</a>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<section>
-				<div class="col-sm-4">
-					<div class="theater border events">
-						<h2 class="title">Teatro</h2>
-						<ul class="list-unstyled">
-							<li>
-								<a href="#">
-									<time class="data">22/03 - </time>
-									<span class="name">Evento 1</span>
-								</a>
-							</li>
-							<li>
-								<a href="#">
-									<time class="data">22/03 - </time>
-									<span class="name">Evento 2</span>
-								</a>
-							</li>
-							<li>
-								<a href="#">
-									<time class="data">22/03 - </time>
-									<span class="name">Evento 3</span>
-								</a>
-							</li>
-							<li>
-								<a href="#">
-									<time class="data">22/03 - </time>
-									<span class="name">Evento 4</span>
-								</a>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</section>
-			<section>
-				<div class="col-sm-4">
-					<div class="show border events">
-						<h2 class="title">Show</h2>
-						<ul class="list-unstyled">
-							<li>
-								<a href="#">
-									<time class="data">22/03 - </time>
-									<span class="name">Evento 1</span>
-								</a>
-							</li>
-							<li>
-								<a href="#">
-									<time class="data">22/03 - </time>
-									<span class="name">Evento 2</span>
-								</a>
-							</li>
-							<li>
-								<a href="#">
-									<time class="data">22/03 - </time>
-									<span class="name">Evento 3</span>
-								</a>
-							</li>
-							<li>
-								<a href="#">
-									<time class="data">22/03 - </time>
-									<span class="name">Evento 4</span>
-								</a>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</section>
-			<section>
-				<div class="col-sm-4">
-					<div class="party border events">
-						<h2 class="title">Festa</h2>
-						<ul class="list-unstyled">
-							<li>
-								<a href="#">
-									<time class="data">22/03 - </time>
-									<span class="name">Evento 1</span>
-								</a>
-							</li>
-							<li>
-								<a href="#">
-									<time class="data">22/03 - </time>
-									<span class="name">Evento 2</span>
-								</a>
-							</li>
-							<li>
-								<a href="#">
-									<time class="data">22/03 - </time>
-									<span class="name">Evento 3</span>
-								</a>
-							</li>
-							<li>
-								<a href="#">
-									<time class="data">22/03 - </time>
-									<span class="name">Evento 4</span>
-								</a>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</section>
+		<div class="row events">
+			<?php
+			// 6 categorias:  Shows - teatro - Dança, literatura - Exposições - Outros
+			include("events/shows.php");
+			include("events/theater.php");
+			include("events/dance.php");
+			echo '<div class="clear"></div>';
+			include("events/literature.php");
+			include("events/exhibition.php");
+			include("events/others.php");
+			?>
 		</div>
 		<div class="row">
 			<div class="col-xs-12">
