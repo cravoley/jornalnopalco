44ecaf8414bc37dfc976d9067c1f1db9f28f9f3b
Valdo Raya <cravoley@gmail.com>
2016-05-07 11:58:22 -0300

    Add dummy events page

---
+++ eventos.php
@@ -0,0 +1,32 @@
+<?php
+include_once("header.php");
+?>
+
+
+    <div class="container content">
+        <div class="col-xs-4 col-sm-2">
+            <div class="row">
+                <div class="col-xs-12">
+                    CALENDARIO
+                    <div class="input-group date datepicker">
+                        <input type="text" class="form-control"><span class="input-group-addon"><i class="glyphicon glyphicon-th"></i></span>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12">
+                    CASAS
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xs-8 col-sm-10">
+            <h1>Conteúdo</h1>
+        </div>
+    </div>
+
+             all
+<?php
+
+
+include "footer.php";
