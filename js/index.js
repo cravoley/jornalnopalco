f711aca8b4318295a2ff71301dec800c24f35cb0
Valdo Raya <cravoley@gmail.com>
2016-05-09 20:31:54 -0300

    ajustes na pagina de eventos

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+require("./menu");
 $(function () {
 	var months = [];
 	$(".datepicker").datepicker(
@@ -6,43 +7,49 @@ $(function () {
 			format: "dd/mm/yyyy",
 			todayBtn: true,
 			language: "pt-BR",
+			maxViewMode:2,
+			clearBtn:true,
+			// multidate:true,
 			autoclose: true,
 			beforeShowMonth:function(date){
 				var year = date.getFullYear();
 				var month = date.getMonth();
-				if(typeof months[year] == "undefined") months[year] = [];
-				if(typeof months[year][month] == "undefined"){
+				if(typeof months[year.toString()] == "undefined") months[year.toString()] = [];
+				if(typeof months[year.toString()][month.toString()] == "undefined"){
+					// months[year][month] = [];
 					// get month, add one prepending a '0' to the string. Get the last 2 chars from the string.
 					// This way, the string "010" will return 10 and "09" will return 09.
 					var userFriendlyMonth = ("0" + (month + 1)).slice(-2);
-					$.getJSON("/jornalnopalco2/api/events/month/" + year + "/" + userFriendlyMonth, function(events){
-						console.log('events', events);
-						// TODO: add event dates to array.
-						// Set special classe for days with events
-					});
+					/* disabled to improve performance
+					$.ajax({
+						dataType: "json",
+						url: "/jornalnopalco2/jpapi/events/month/" + year + "/" + userFriendlyMonth,
+						async:false, // runs syncronous because we need to assure that the array has it values beforeShowDay
+						success: function(events){
+							months[year.toString()][month.toString()] = events;
+						}
+					});*/
 				}
 				// this is called for everymonth in Year
 				// $.getJSON("/api/events/daysInMonth");
 				return true;
+			},
+			beforeShowDay: function (date){
+				var month = date.getMonth();
+				var year = date.getFullYear();
+				if(Array.isArray(months)){
+					if(Array.isArray(months[year]) && Array.isArray(months[year][month])){
+						if(months[year][month].indexOf(date.getDate()) > -1){
+							return {
+								classes: 'active'
+							};
+						}
+					}
+				}
 			}
-			// ,
-			// beforeShowDay: function (date){
-			// 	if (date.getMonth() == (new Date()).getMonth())
-			// 		switch (date.getDate()){
-			// 			case 4:
-			// 				return {
-			// 					tooltip: 'Example tooltip',
-			// 					classes: 'active'
-			// 				};
-			// 			case 8:
-			// 				return false;
-			// 			case 12:
-			// 				return "green";
-			// 		}
-			// }
 		}
 	).on('changeDate', function (e){
 		// change page to fetch all events for the selected day
-		console.log(e.date.getTime());
+		// console.log(e.date.getTime());
 	});
 });
