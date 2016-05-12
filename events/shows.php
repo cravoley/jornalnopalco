aa56cf03984f89053244f92d52fd810c93733805
Valdo Raya <cravoley@gmail.com>
2016-05-11 21:36:16 -0300

    update styles

--- events/shows.php
+++ events/shows.php
@@ -1,25 +1,3 @@
-<section>
-    <div class="col-sm-4">
-        <div class="show border">
-            <h2 class="title">Show</h2>
-            <ul class="list-unstyled">
-                <?php
-                    for($i=0;$i<6;$i++){
-                        ?>
-
-                        <li>
-                           <a href="#">
-                               <time class="data">22/03 - </time>
-                               <span class="name">Evento <?php echo $i+1;?></span>
-                           </a>
-                        </li>
-                        <?php
-                    }
-                 ?>
-            </ul>
-            <div class="more pull-right">
-                <a href="eventos.php">Ver mais</a>
-            <div>
-        </div>
-    </div>
-</section>
+<?php
+    printNextEvents("show");
+ ?>
