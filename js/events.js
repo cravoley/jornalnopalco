565344d8434e272bc95cd1150f6e7e8501909176
Valdo Raya <cravoley@gmail.com>
2016-05-12 22:26:57 -0300

    add jquery functions to handle date filtering

--- js/events.js
+++ js/events.js
@@ -3,13 +3,13 @@
         var toFilter = [];
         var toUnfilter = [];
         var filterBlock = $(".filter");
+        var selectedDate;
         var filterButton = $(filterBlock).find("input.update");
         $(filterBlock).find(".place-filter").find("li").find("label").click(function(e){
             var targetTag = e.target.tagName;
             if(targetTag == "INPUT") return; // prevent extra events fired
 
             var place = $(this).data("place").toString(); //assure we are handling only with strings
-            console.log(toUnfilter, toFilter, place);
             if(targetTag == "A" || targetTag == "i"){
                 // unfiltering
                 if($(this).hasClass("removed")){
@@ -28,9 +28,15 @@
                 }
             }
 
-            var disabled = (toFilter.length > 0 || toUnfilter.length > 0?null:"disabled");
-            filterButton.attr("disabled",disabled);
+            updateFilterButton();
         });
+
+        function updateFilterButton(){
+            var disabled = (toFilter.length > 0 || toUnfilter.length > 0 || selectedDate instanceof Date ? null : "disabled");
+            filterButton.attr("disabled",disabled);
+        }
+
+
         $(filterButton).click(function(e){
             // disable form and triggers
             $(filterBlock).css("opacity","0.5")
@@ -48,15 +54,30 @@
                         }
                     }
                 });
+            var searchString = "";
             // length should always be greater than 0 but, who knows?
             if(toSearch.length > 0){
                 var searchString = toSearch
                     .map((i) => "place[]=" + i)
                     .join("&");
-                    console.log(searchString);
-                window.location.search = searchString;
-            } else
-                window.location.search = "";
+            }
+
+            if(selectedDate instanceof Date){
+                if(searchString.length > 0){
+                    searchString += "&";
+                }
+                searchString += "selectedDate="
+                    + selectedDate.getFullYear()
+                    + ("0" + (selectedDate.getMonth() + 1).toString()).slice(-2)
+                    + ("0" + selectedDate.getDate().toString()).slice(-2);
+            }
+            window.location.search = searchString;
+            return;
+            //
+            // if(selectedDate.size() > 0){
+            //     selectedDate = selectedDate[0].getDate();
+            // }
+            // TODO: handle datepicker date
         });
 
 
@@ -71,15 +92,15 @@
     			clearBtn:true,
     			// multidate:true,
     			autoclose: true,
-    			beforeShowMonth:function(date){
-    				var year = date.getFullYear();
-    				var month = date.getMonth();
-    				if(typeof months[year.toString()] == "undefined") months[year.toString()] = [];
-    				if(typeof months[year.toString()][month.toString()] == "undefined"){
+    			// beforeShowMonth:function(date){
+    			// 	var year = date.getFullYear();
+    			// 	var month = date.getMonth();
+    			// 	if(typeof months[year.toString()] == "undefined") months[year.toString()] = [];
+    			// 	if(typeof months[year.toString()][month.toString()] == "undefined"){
     					// months[year][month] = [];
     					// get month, add one prepending a '0' to the string. Get the last 2 chars from the string.
     					// This way, the string "010" will return 10 and "09" will return 09.
-    					var userFriendlyMonth = ("0" + (month + 1)).slice(-2);
+    					// var userFriendlyMonth = ("0" + (month + 1)).slice(-2);
     					/* disabled to improve performance
     					$.ajax({
     						dataType: "json",
@@ -89,11 +110,11 @@
     							months[year.toString()][month.toString()] = events;
     						}
     					});*/
-    				}
+    				// }
     				// this is called for everymonth in Year
     				// $.getJSON("/api/events/daysInMonth");
-    				return true;
-    			},
+    				// return true;
+    			// },
     			beforeShowDay: function (date){
     				var month = date.getMonth();
     				var year = date.getFullYear();
@@ -109,8 +130,27 @@
     			}
     		}
     	).on('changeDate', function (e){
+            var oldSelected = selectedDate;
+            if(e.date)
+                selectedDate = e.date
+            else
+                selectedDate = "undefined";
+
+            // if they are equally undefined or have the same time, do not update
+            if(oldSelected == selectedDate
+                || (
+                    oldSelected instanceof Date
+                    && selectedDate instanceof Date
+                    && oldSelected.getTime() == selectedDate.getTime()
+                )
+            )
+            console.log("equals");
+            else
+                updateFilterButton();
     		// change page to fetch all events for the selected day
     		// console.log(e.date.getTime());
     	});
+        // set default value
+        selectedDate = $(".datepicker").datepicker("getDate");
     });
 })(jQuery);
