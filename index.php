8ea3c107ece93f9eae16971d9af6c9347c7ef71e
Valdo Raya <cravoley@gmail.com>
2016-05-26 14:43:36 -0300

    setup configuration var to delivery backend content to react

--- index.php
+++ index.php
@@ -1,21 +1,23 @@
 <?php
 include_once("header.php");
 ?>
+<div id="content"></div>
+
 <div class="">
     <div class="container content">
 
-        <div id="content"></div>
+
 
         <div class="row events">
             <?php
             // 6 categorias:  Shows - teatro - Dança, literatura - Exposições - Outros
-            printNextEvents("show", "Shows");
-            printNextEvents("teatro", "Teatros");
-            printNextEvents("danca","Danças");
-            echo '<div class="clear"></div>';
-            printNextEvents("literatura", "Literatura");
-            printNextEvents("exposicao", "Exposições");
-            printNextEvents("outros", "Outros");
+            // printNextEvents("show", "Shows");
+            // printNextEvents("teatro", "Teatros");
+            // printNextEvents("danca","Danças");
+            // echo '<div class="clear"></div>';
+            // printNextEvents("literatura", "Literatura");
+            // printNextEvents("exposicao", "Exposições");
+            // printNextEvents("outros", "Outros");
 
             //budo
             //http://webpack.github.io/docs/what-is-webpack.html
@@ -25,10 +27,15 @@ include_once("header.php");
     </div>
 </div>
 <script>
-    var configuration={
-        isSingle:'<?php echo is_single()?"true":"false";?>',
-        id:'<?php echo get_the_id();?>'
-    };
+<?php
+    $configuration = array();
+    $configuration["isSingle"] = is_single() ? true : false;
+    $configuration["isPage"] = is_post_type_archive() ? true : get_post_type() == 'page' ? true : false;
+    $configuration["page"] = get_post_type() == 'page' ? get_post()->post_name : get_post_type();
+    $configuration['templateUrl'] = get_template_directory_uri();
+    $configuration['baseUrl'] = get_site_url();
+    $configuration["id"] = get_the_id() ? get_the_id() : "";
+?>
+    var configuration = <?php echo json_encode($configuration); ?>
 </script>
-
 <?php include_once("footer.php"); ?>
