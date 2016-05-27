1b253ff0a61e2335b1b36eb1046a0b183797a76c
Valdo Raya <cravoley@gmail.com>
2016-05-27 20:58:29 -0300

    add animate css

--- header.php
+++ header.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker3.min.css">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/bower_components/jquery-ui/themes/black-tie/theme.css">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/bower_components/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/bower_components/animate/animate.min.css">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/dist/css/index.css">
 </head>
 <body class="largeMenu">
