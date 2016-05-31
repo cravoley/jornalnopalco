9f91c23b82e9b218d730fb6b07098f69604b2af7
Valdo Raya <cravoley@gmail.com>
2016-05-31 09:42:04 -0300

    add GA keys

--- index.php
+++ index.php
@@ -36,6 +36,26 @@ include_once("header.php");
     $configuration['baseUrl'] = get_site_url();
     $configuration["id"] = get_the_id() ? get_the_id() : "";
 ?>
+    var html5lightbox_options = {
+        "freelink": "",
+        "googleanalyticsaccount": "<?php echo GA_KEY;?>",
+        "barheight": 0
+    };
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', '<?php echo GA_KEY;?>', 'auto');
+    ga('require', 'displayfeatures');
+    ga('send', 'pageview');
     var configuration = <?php echo json_encode($configuration); ?>
 </script>
 <?php include_once("footer.php"); ?>
