d903e3f78c1011b54f25ee505b108da1b23190a7
Valdo Raya <cravoley@gmail.com>
2016-05-31 09:41:47 -0300

    add html5lightbox

--- footer.php
+++ footer.php
@@ -30,9 +30,11 @@
 <script src="<?php echo get_template_directory_uri();?>/bower_components/jquery-ui/jquery-ui.min.js"></script>
 <script src="<?php echo get_template_directory_uri();?>/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 <script src="<?php echo get_template_directory_uri();?>/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+<script src="<?php echo get_template_directory_uri();?>/js/native.history.js"></script>
+<script type="text/javascript" src="<?php echo get_template_directory_uri() . '/lib/html5lightbox/html5lightbox.js'; ?>" async></script>
 <!-- <script src="dist/js/all.js"></script> -->
 <!-- TODO -->
-<!-- <script src="<?php echo get_template_directory_uri();?>/dist/js/bundle.js"></script> -->
+<!-- <script src="<?php echo get_template_directory_uri();?>/dist/bundle.js"></script> -->
 <script src="http://localhost:3000/assets/bundle.js"></script>
 </body>
 </html>
