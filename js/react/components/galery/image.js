628913406d28b1cf262f39eb03259b666d9160d3
Valdo Raya <cravoley@gmail.com>
2016-06-03 15:23:07 -0300

    add animation

--- js/react/components/galery/image.js
+++ js/react/components/galery/image.js
@@ -10,16 +10,18 @@ export default class GalleryImage extends Component{
         let { thumb, title, text, image } = this.props;
 
         return(
-            <div class="thumb col-xs-3 image">
+            <div class="col-xs-2 image animated zoomIn">
+                <div class="thumb image">
                 <a
                     href={image}
                     data-group="gallery"
                     data-thumbnail={thumb}
                     data-description={title}
                     class="html5lightbox imageInside">
-                    <img class="img-responsive" src={thumb} alt={title} />
+                    <img class="img-responsive" src={thumb} alt={title}} />
                     <span class="thumbLabel sr-only">{title}</span>
                 </a>
+                </div>
             </div>
         )
     }
