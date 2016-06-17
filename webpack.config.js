36c4368cdbbdd913c429857892a9b251d47f3b07
Valdo Raya <cravoley@gmail.com>
2016-06-17 15:47:09 -0300

    set webpack pack resolver

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,28 @@
 var path = require("path");
 var webpack = require("webpack");
+console.log(path.resolve('./js/react'));
 module.exports = {
   entry: {
     app: [
     'webpack-hot-middleware/client?reload=true'
-    ,"./js/react/index.js"]
+    ,"./js/react/index.js"],
+    // vendor: [
+    //   './webpack.init.js',
+    // ]
   },
   output: {
     path: path.resolve(__dirname, "dist"),
     publicPath: "/assets/",
     filename: "bundle.js"
 },
+resolve: {
+    extensions: ['', '.js', '.jsx'],
+    root: [
+      path.resolve('./js/react'),
+      'node_modules'
+    ]
+}
+,
  plugins: [
      new webpack.HotModuleReplacementPlugin(),
      new webpack.ProvidePlugin({
@@ -29,10 +41,12 @@ module.exports = {
         "presets": ["es2015","react", "stage-0", "react-hmre"],
          "plugins": ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
       }
-    }, {
+    },
+    {
       test: /\.json?$/,
       loader: 'json'
-    }, {
+    },
+    {
       test: /\.css$/,
       loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]'
     }]
